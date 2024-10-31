1e18562b6c5f90a72b2d1f106e2a92f1ddbf6b44
Stoyan Vuchev <stoyan.v.vuchev@gmail.com>
2024-10-31 05:26:40 +0200

    Added Crypto Preferences.
    - Added CryptoPreferences.kt
    - Added CryptoPreferencesImpl.kt
    - Added CryptoPreferencesTest.kt
    - Added CryptoPreferencesError.kt
    - Added CryptoPreferencesUtils.kt
    - Other fixes & improvements

--- app/src/main/java/com/stoyanvuchev/cryptotracker/crypto/data/repository/CoinRepositoryImpl.kt
+++ app/src/main/java/com/stoyanvuchev/cryptotracker/crypto/data/repository/CoinRepositoryImpl.kt
@@ -1,82 +1,85 @@
 package com.stoyanvuchev.cryptotracker.crypto.data.repository
 
 import com.stoyanvuchev.cryptotracker.core.data.repository.CoinRepositoryError
+import com.stoyanvuchev.cryptotracker.core.domain.networking.util.NetworkError
 import com.stoyanvuchev.cryptotracker.core.domain.util.Error
 import com.stoyanvuchev.cryptotracker.core.domain.util.Result
+import com.stoyanvuchev.cryptotracker.core.domain.util.onError
+import com.stoyanvuchev.cryptotracker.core.domain.util.onSuccess
 import com.stoyanvuchev.cryptotracker.crypto.data.local.LocalDatabaseDao
 import com.stoyanvuchev.cryptotracker.crypto.data.mappers.toCoin
 import com.stoyanvuchev.cryptotracker.crypto.domain.model.Coin
 import com.stoyanvuchev.cryptotracker.crypto.domain.networking.RemoteDataSource
+import com.stoyanvuchev.cryptotracker.crypto.domain.preferences.CryptoPreferences
 import com.stoyanvuchev.cryptotracker.crypto.domain.repository.CoinRepository
+import kotlin.time.Duration.Companion.hours
 
 /**
- * Implementation of [CoinRepository] that manages data retrieval and storage for coins.
+ * Implementation of [CoinRepository] responsible for managing data retrieval and storage for cryptocurrency coins.
  *
- * This class fetches data from a [RemoteDataSource] and caches it in a local database using
- * [LocalDatabaseDao]. It first attempts to fetch data from the local database and, if unavailable,
- * retrieves data from the remote source and stores it locally.
+ * This repository prioritizes retrieving data from a local cache ([LocalDatabaseDao]) for efficiency and only fetches
+ * from the remote source ([RemoteDataSource]) if the local data is outdated or unavailable. Cached data is updated
+ * periodically to keep it in sync with remote data, and preferences ([CryptoPreferences]) are used to track the last
+ * successful remote data fetch.
  *
- * @property remoteDataSource The data source for remote API interactions.
- * @property localDatabaseDao The data access object for local database CRUD operations.
+ * @property remoteDataSource The source for remote API interactions related to cryptocurrency data.
+ * @property localDatabaseDao Data access object for performing local database operations.
+ * @property cryptoPreferences Data source for storing and retrieving timestamp of the last successful remote fetch.
  */
 class CoinRepositoryImpl(
     private val remoteDataSource: RemoteDataSource,
-    private val localDatabaseDao: LocalDatabaseDao
+    private val localDatabaseDao: LocalDatabaseDao,
+    private val cryptoPreferences: CryptoPreferences
 ) : CoinRepository {
 
     /**
-     * Retrieves a list of all coins, first checking the local database, then fetching from remote if necessary.
+     * Retrieves a list of all cryptocurrency coins. This method checks the local database for cached data first,
+     * only fetching from the remote source if data is missing or outdated (older than one hour).
      *
-     * If the coins are available in the local database, they are returned. Otherwise, the method fetches the
-     * data from the [remoteDataSource], inserts it into the local database, and then retrieves and returns it.
-     * Returns an error result if the data cannot be retrieved from either source.
+     * Data retrieved from the remote source is cached locally for future calls. If neither local nor remote data
+     * is available, an error is returned.
      *
-     * @return A [Result] containing a list of [Coin] if successful, or a [CoinRepositoryError] if an error occurs.
+     * @return A [Result] containing a list of [Coin] objects if successful, or a [CoinRepositoryError] if an error occurs.
      */
     override suspend fun getAllCoins(): Result<List<Coin>, Error> {
         return try {
 
-            val coinsList = localDatabaseDao.getPaginatedCoins()
-            if (!coinsList.isNullOrEmpty()) {
+            val oneHourAgo = System.currentTimeMillis().hours.minus(1.hours).inWholeMilliseconds
+            when (val timestampResult = getLastFetchedTimestamp()) {
 
-                Result.Success(coinsList.map { it.toCoin() })
+                is Result.Success -> {
 
-            } else {
+                    if (timestampResult.data == null || oneHourAgo <= timestampResult.data) {
+                        fetchCoinsFromRemoteDataSource()
+                    } else {
 
-                when (val result = remoteDataSource.getCoins()) {
-
-                    is Result.Success -> {
-
-                        localDatabaseDao.insertCoins(result.data)
-
-                        val newCoins = localDatabaseDao.getPaginatedCoins()
-                        if (!newCoins.isNullOrEmpty()) {
-                            Result.Success(newCoins.map { it.toCoin() })
+                        val coinsList = localDatabaseDao.getPaginatedCoins()
+                        if (!coinsList.isNullOrEmpty()) {
+                            Result.Success(coinsList.map { it.toCoin() })
                         } else {
-                            Result.Error(CoinRepositoryError.Unknown)
+                            fetchCoinsFromRemoteDataSource()
                         }
 
                     }
 
-                    is Result.Error -> result
-
                 }
 
-            }
+                is Result.Error -> Result.Error(timestampResult.error)
 
+            }
         } catch (e: Exception) {
             Result.Error(CoinRepositoryError.Unknown)
         }
     }
 
     /**
-     * Retrieves a specific coin by its symbol, checking the local database first.
+     * Retrieves a specific cryptocurrency coin by its symbol from the local database.
      *
-     * If the requested coin is not found in the local database, returns an error. This method also
-     * returns an error result if an unexpected issue occurs.
+     * If the coin is not found locally, an error is returned. This method only checks the local cache and does
+     * not attempt to fetch data from the remote source.
      *
-     * @param symbol The symbol of the coin to retrieve.
-     * @return A [Result] containing the requested [Coin] if successful, or a [CoinRepositoryError] if an error occurs.
+     * @param symbol The unique symbol of the coin to retrieve.
+     * @return A [Result] containing the requested [Coin] object if found, or a [CoinRepositoryError] if not found or an error occurs.
      */
     override suspend fun getCoinBySymbol(symbol: String): Result<Coin, Error> {
         return try {
@@ -91,4 +94,60 @@ class CoinRepositoryImpl(
         }
     }
 
+    /**
+     * Retrieves the timestamp of the last successful data fetch from the remote source.
+     *
+     * @return A [Result] containing the timestamp as a [Long] if successful, or an [Error] if unable to retrieve the data.
+     */
+    private suspend fun getLastFetchedTimestamp(): Result<Long?, Error> {
+        return try {
+            cryptoPreferences
+                .getLastFetchedTimestamp()
+                .onSuccess { timestamp -> Result.Success(timestamp) }
+                .onError { error -> Result.Error(error) }
+        } catch (e: Exception) {
+            Result.Error(CoinRepositoryError.Unknown)
+        }
+    }
+
+    /**
+     * Fetches a list of coins from the remote source and updates the local cache if the fetch is successful.
+     *
+     * After a successful fetch, the current timestamp is recorded, and the new data is stored in the local database.
+     * If an error occurs during remote fetch or data insertion, an error result is returned.
+     *
+     * @return A [Result] containing a list of [Coin] objects if successful, or a [CoinRepositoryError] if an error occurs.
+     */
+    private suspend fun fetchCoinsFromRemoteDataSource(): Result<List<Coin>, Error> {
+        return try {
+            when (val result = remoteDataSource.getCoins()) {
+                is Result.Success -> {
+
+                    val now = System.currentTimeMillis()
+                    when (val timestampResult = cryptoPreferences.setLastFetchedTimestamp(now)) {
+
+                        is Result.Success -> {
+
+                            localDatabaseDao.insertCoins(result.data)
+
+                            val newCoins = localDatabaseDao.getPaginatedCoins()
+                            if (!newCoins.isNullOrEmpty()) {
+                                Result.Success(newCoins.map { it.toCoin() })
+                            } else {
+                                Result.Error(CoinRepositoryError.Unknown)
+                            }
+
+                        }
+
+                        is Result.Error -> Result.Error(timestampResult.error)
+
+                    }
+                }
+
+                is Result.Error -> result
+            }
+        } catch (e: Exception) {
+            Result.Error(NetworkError.Unknown)
+        }
+    }
 }
